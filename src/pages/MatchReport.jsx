f0cc3f9eb6d20a567aca4215cd0f1f001d3ef788
mcogaha-star <macconghamhna@gmail.com>
2026-03-29 14:23:27 +0100

    Fix report hooks import

--- src/pages/MatchReport.jsx
+++ src/pages/MatchReport.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { ArrowLeft, BarChart3, ChevronDown } from 'lucide-react';
